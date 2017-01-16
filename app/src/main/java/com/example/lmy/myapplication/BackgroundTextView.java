59209126e2825d438e5209ab26cd69faf938c6c4
limengying <limengying@qiyi.com>
2017-01-16 09:58:40 +0800

    添加原型

---
+++ app/src/main/java/com/example/lmy/myapplication/BackgroundTextView.java
@@ -0,0 +1,45 @@
+package com.example.lmy.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by limengying on 2017/1/14.
+ */
+
+public class BackgroundTextView extends TextView {
+    Paint mPaint1;
+    Paint mPaint2;
+
+    public BackgroundTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint1 = new Paint();
+        mPaint1.setColor(getResources().getColor(android.R.color.holo_blue_light));
+        mPaint1.setStyle(Paint.Style.FILL);
+        mPaint2 = new Paint();
+        mPaint2.setColor(Color.YELLOW);
+        mPaint2.setStyle(Paint.Style.FILL);
+    }
+
+    public BackgroundTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BackgroundTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(0,0,getMeasuredWidth(),getMeasuredHeight(),mPaint1);
+        canvas.drawRect(10,10,getMeasuredWidth()-10,getMeasuredHeight()-10,mPaint2);
+        canvas.save();
+        canvas.translate(100,0);
+        super.onDraw(canvas);
+        canvas.restore();
+    }
+}
---
+++ app/src/main/java/com/example/lmy/myapplication/ShaderTextView.java
@@ -0,0 +1,64 @@
+package com.example.lmy.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by limengying on 2017/1/14.
+ */
+
+public class ShaderTextView extends TextView {
+    int mViewWidth = 0;
+    float mTranslate = 0;
+    Paint mPaint;
+    LinearGradient mLinearGradient;
+    Matrix mGradientMatrix;
+    public ShaderTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ShaderTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    public ShaderTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if(mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if(mViewWidth >0){
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0,0,mViewWidth,0,new int[]{Color.BLUE,0xffffff,Color.BLUE},null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if(mGradientMatrix !=null) {
+            mTranslate += mViewWidth/5;
+            if(mTranslate>2*mViewWidth) {
+                mTranslate-=mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate,0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
